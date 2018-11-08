6785cc2ec10de8273f8570326782ac5ec0a219dd
Marc André Freiheit <marcandre@freiheit.software>
2018-11-08 17:47:22 +0100

    Add test for author index

---
+++ spec/features/author/index_author_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "Index author page", type: :feature do
+  
+  before :each do
+    @alan = FactoryBot.create :author
+  end
+
+  it 'should render withour error' do
+    visit authors_path
+  end
+
+  it 'should have a table for all authors' do
+    visit authors_path
+
+    expect(page).to have_text 'Name'
+    expect(page).to have_text 'Homepage'
+  end
+
+  it 'should display the first name, last name and homepage of Alan Turing' do
+    visit authors_path 
+
+    expect(page).to have_text 'Alan'
+    expect(page).to have_text 'Turing'
+    expect(page).to have_text 'http://wikipedia.de/Alan_Turing'
+  end
+end
+
