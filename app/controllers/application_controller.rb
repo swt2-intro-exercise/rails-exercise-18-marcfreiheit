ecf741aa46b0de15a81d742baa80d70a5c9e4838
Marc André Freiheit <marcandre@freiheit.software>
2018-11-08 13:17:23 +0100

    Add author form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def create
+    @article = Author.new author_params
+     
+    @article.save
+    #redirect_to @article
+  end
+
+  private
+    def author_params
+      params.require(:author).permit(:first_name,:last_name,:homepage)
+    end
 end
