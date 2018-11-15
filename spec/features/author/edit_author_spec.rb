40c82e38f1f71d8e00f2c1212efe3344de4e2d6e
Marc André Freiheit <marcandre@freiheit.software>
2018-11-15 19:44:51 +0100

    Add test to validate edit and update of author

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -4,10 +4,26 @@ describe "Edit author page", type: :feature do
   
   before :each do
     @alan = FactoryBot.create :author
+    visit edit_author_path(@alan)
   end
 
   it "should render withour error" do
-    visit edit_author_path(@alan)
+  end
+
+  it 'should have input fields to edit first name, last name and home page' do
+    expect(page).to have_field 'author[first_name]'
+    expect(page).to have_field 'author[last_name]'
+    expect(page).to have_field 'author[homepage]'
+  end
+
+  it 'should persit updates on name on an author' do
+    page.fill_in 'author[first_name]', with: 'Alan Mathison'
+
+    find('input[type="submit"]').click
+    
+    @alan.reload
+
+    expect(@alan.first_name).to eq 'Alan Mathison'
   end
 
 end
