283bba69e684267349606d1276218246b97c1dac
Marc André Freiheit <marcandre@freiheit.software>
2018-11-15 19:45:06 +0100

    Add edit and update of author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,6 +3,10 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
+  def edit
+    @author = Author.find params[:id]
+  end
+
   def create
     @author = Author.new author_params
  
@@ -13,7 +17,14 @@ class AuthorsController < ApplicationController
     end
   end
 
-  def edit
+  def update
+    @author = Author.find params[:id]
+    
+    if @author.update author_params
+     redirect_to @author
+    else
+     render 'edit' 
+    end
   end
 
   def show
